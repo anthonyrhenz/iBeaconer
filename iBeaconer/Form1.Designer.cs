0bbd38d85262ef652cfccdc3c487cebed0b1175e
Anthony Bennett <anton8000@live.com>
2019-01-22 10:44:47 +0000

    Add project files.

---
+++ iBeaconer/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace iBeaconer
+{
+    partial class mainWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWin));
+            this.execute = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.uuid = new System.Windows.Forms.TextBox();
+            this.major = new System.Windows.Forms.TextBox();
+            this.minor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.roomSelect = new System.Windows.Forms.ComboBox();
+            this.indicator = new System.Windows.Forms.Label();
+            this.helpTip = new System.Windows.Forms.ToolTip(this.components);
+            this.stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // execute
+            // 
+            this.execute.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.execute.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.execute.Location = new System.Drawing.Point(38, 117);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(125, 23);
+            this.execute.TabIndex = 5;
+            this.execute.Text = "Fire!";
+            this.execute.UseVisualStyleBackColor = true;
+            this.execute.Click += new System.EventHandler(this.execute_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Help;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(75, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Anthony\'s iBeacon Broacasting Tool";
+            this.helpTip.SetToolTip(this.label1, "Created Oct 2018 by Anthony | v1.1");
+            // 
+            // uuid
+            // 
+            this.uuid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.uuid.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.uuid.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.uuid.Location = new System.Drawing.Point(38, 47);
+            this.uuid.MaxLength = 47;
+            this.uuid.Name = "uuid";
+            this.uuid.Size = new System.Drawing.Size(288, 13);
+            this.uuid.TabIndex = 2;
+            this.uuid.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uuid.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.uuid_KeyPress);
+            // 
+            // major
+            // 
+            this.major.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.major.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.major.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.major.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.major.Location = new System.Drawing.Point(38, 84);
+            this.major.MaxLength = 5;
+            this.major.Name = "major";
+            this.major.Size = new System.Drawing.Size(125, 13);
+            this.major.TabIndex = 3;
+            this.major.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.major.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.major_KeyPress);
+            // 
+            // minor
+            // 
+            this.minor.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.minor.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.minor.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.minor.Location = new System.Drawing.Point(201, 84);
+            this.minor.MaxLength = 5;
+            this.minor.Name = "minor";
+            this.minor.Size = new System.Drawing.Size(125, 13);
+            this.minor.TabIndex = 4;
+            this.minor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.minor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.minor_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(146, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 99;
+            this.label2.Text = "Room UUID";
+            this.helpTip.SetToolTip(this.label2, "Click to Paste Clipboard");
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(72, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(223, 16);
+            this.label3.TabIndex = 99;
+            this.label3.Text = "Major Pair                                    Minor Pair";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // roomSelect
+            // 
+            this.roomSelect.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.roomSelect.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.roomSelect.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.roomSelect.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.roomSelect.FormattingEnabled = true;
+            this.roomSelect.Location = new System.Drawing.Point(38, 190);
+            this.roomSelect.Name = "roomSelect";
+            this.roomSelect.Size = new System.Drawing.Size(288, 24);
+            this.roomSelect.TabIndex = 8;
+            this.roomSelect.Text = "Beacon Presets";
+            this.helpTip.SetToolTip(this.roomSelect, "Presets for the University of Exeter");
+            this.roomSelect.SelectedIndexChanged += new System.EventHandler(this.roomSelect_SelectedIndexChanged);
+            this.roomSelect.Click += new System.EventHandler(this.roomSelect_Click);
+            // 
+            // indicator
+            // 
+            this.indicator.AutoSize = true;
+            this.indicator.BackColor = System.Drawing.Color.Transparent;
+            this.indicator.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.indicator.ForeColor = System.Drawing.Color.Red;
+            this.indicator.Location = new System.Drawing.Point(90, 155);
+            this.indicator.Name = "indicator";
+            this.indicator.Size = new System.Drawing.Size(188, 16);
+            this.indicator.TabIndex = 99;
+            this.indicator.Text = "████████████████████";
+            this.indicator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // stop
+            // 
+            this.stop.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.stop.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.stop.Location = new System.Drawing.Point(201, 117);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(125, 23);
+            this.stop.TabIndex = 6;
+            this.stop.Text = "Cease";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // mainWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImage = global::iBeaconer.Properties.Resources.bgwin;
+            this.ClientSize = new System.Drawing.Size(362, 226);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.indicator);
+            this.Controls.Add(this.roomSelect);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.minor);
+            this.Controls.Add(this.major);
+            this.Controls.Add(this.uuid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.execute);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainWin";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "iBeaconer by Anthony";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button execute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox uuid;
+        private System.Windows.Forms.TextBox major;
+        private System.Windows.Forms.TextBox minor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox roomSelect;
+        private System.Windows.Forms.Label indicator;
+        private System.Windows.Forms.ToolTip helpTip;
+        private System.Windows.Forms.Button stop;
+    }
+}
+
---
+++ iBeaconer/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+
+//bonus imports ;)
+//using Windows.Storage;
+//using Windows.Data;
+//using Windows.Services;
+using Windows.Storage.Streams;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.Background;
+
+namespace iBeaconer
+{
+    public partial class mainWin : Form
+    {
+        public BluetoothLEAdvertisementPublisher publisher;
+
+        public List<string> names { get; set; } = new List<string>();
+        public List<string> uuids { get; set; } = new List<string>();
+        public List<string> majorpair { get; set; } = new List<string>();
+        public List<string> minorpair { get; set; } = new List<string>();
+
+        public mainWin()
+        {
+            InitializeComponent();
+
+            /*
+            using (var Reader = new StreamReader(@".\positions.csv"))
+            {
+                List<string> names = new List<string>();
+                while (!Reader.EndOfStream)
+                {
+                    var line = Reader.ReadLine();
+                    var values = line.Split(',');
+
+                    names.Add(values[0]);
+                    uuids.Add(values[1]);
+                    majorpair.Add(values[2]);
+                    minorpair.Add(values[3]);
+                }
+
+
+                roomSelect.DataSource = names;
+                Debug.WriteLine(names[3]);
+                Debug.WriteLine(majorpair[3]);
+                Debug.WriteLine(minorpair[3]);
+            }
+            */
+
+            string[] csvPos = Properties.Resources.positions.Split('\n');
+            foreach (string s in csvPos)
+            {
+                var values = s.Split(',');
+                names.Add(values[0]);
+                uuids.Add(values[1]);
+                majorpair.Add(values[2]);
+                minorpair.Add(values[3]);
+            }
+
+            //moved all init to func
+
+            
+                //create manufacturer UUID
+                ////drastic updates.
+                /*
+                var writer = new DataWriter();
+                UInt16 uuidData = 0x1234;
+                writer.WriteUInt16(uuidData);
+
+                manufacturerData.Data = writer.DetachBuffer();
+                //add manufacturer data to our gun
+                publisher.Advertisement.ManufacturerData.Add(manufacturerData);
+
+                ///now we create our payload.
+                //payload = new BluetoothLEAdvertisement();
+                Guid guid = Guid.Parse("00001804-0000-1000-8000-00805f9b34fb");
+                publisher.Advertisement.ServiceUuids.Add(guid);
+
+                publisher.Advertisement.Flags = BluetoothLEAdvertisementFlags.GeneralDiscoverableMode;
+                */
+            
+
+            
+        }
+
+        public void execute_Click(object sender, EventArgs e) //it's time.
+        {
+
+            //begin construction for advert
+            BluetoothLEAdvertisementPublisher publisher = new BluetoothLEAdvertisementPublisher();
+
+            //create/set manufacturer ID
+            var manufacturerData = new BluetoothLEManufacturerData();
+            manufacturerData.CompanyId = 0x004c; //apple inc's company data
+
+            if (major.Text==string.Empty || minor.Text==string.Empty || uuid.Text==string.Empty)
+            {
+                MessageBox.Show("ur autistic, put some values in.");
+                return;
+            }
+
+            //process our user input data
+            var majorhex = Convert.ToInt32(major.Text).ToString("X").PadLeft(4,'0');
+            var minorhex = Convert.ToInt32(minor.Text).ToString("X").PadLeft(4,'0');
+            string maj1 = Convert.ToString(majorhex[0])+majorhex[1];
+            string maj2 = Convert.ToString(majorhex[2])+majorhex[3];
+            string min1 = Convert.ToString(minorhex[0])+minorhex[1];
+            string min2 = Convert.ToString(minorhex[2])+minorhex[3];
+
+            string[] hold = uuid.Text.Split(' ');
+
+            // Create the payload
+            //harrison004: f0207da6-4fa2-4e98-8024-bc5b71e0893e  |  6  |  11
+            // f0 20 7d a6 4f a2 4e 98 80 24 bc 5b 71 e0 89 3e
+
+            //harrison 209
+            // f0 20 7d a6 4f a2 4e 98 80 24 bc 5b 71 e0 89 3e  |  6  |  179
+            var writer = new DataWriter();
+
+            byte[] dataArray = new byte[23];
+            int i = 2;
+            dataArray[0] = byte.Parse("02", System.Globalization.NumberStyles.AllowHexSpecifier);
+            dataArray[1] = byte.Parse("15", System.Globalization.NumberStyles.AllowHexSpecifier);
+            foreach (string item in hold)
+            {
+                dataArray[i++] = byte.Parse(item, System.Globalization.NumberStyles.AllowHexSpecifier);
+            }
+            dataArray[18] = byte.Parse(maj1, System.Globalization.NumberStyles.AllowHexSpecifier);
+            dataArray[19] = byte.Parse(maj2, System.Globalization.NumberStyles.AllowHexSpecifier);
+            dataArray[20] = byte.Parse(min1, System.Globalization.NumberStyles.AllowHexSpecifier);
+            dataArray[21] = byte.Parse(min2, System.Globalization.NumberStyles.AllowHexSpecifier);
+            dataArray[22] = byte.Parse("B3", System.Globalization.NumberStyles.AllowHexSpecifier);
+
+            /*
+            byte[] dataArray = new byte[] {
+                // last 2 bytes of Apple's iBeacon
+                0x02, 0x15,
+                // UUID e2 c5 6d b5 df fb 48 d2 b0 60 d0 f5 a7 10 96 e0
+                0xf0, 0x20, 0x7d, 0xa6,
+                0x4f, 0xa2, 0x4e, 0x98,
+                0x80, 0x24, 0xbc, 0x5b,
+                0x71, 0xe0, 0x89, 0x3e,
+                // Major
+                0x00, 0x06,
+                // Minor
+                0x00, 0x0B,
+                // TX power
+                0xc5
+            };
+            */
+
+            writer.WriteBytes(dataArray);
+
+            manufacturerData.Data = writer.DetachBuffer();
+            publisher.Advertisement.ManufacturerData.Add(manufacturerData);
+
+
+            publisher.Start(); //begin the spoof ;)
+
+            indicator.ForeColor = Color.Green;
+        }
+
+        public void stop_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You're welcome ;)");
+            Application.Exit();
+
+            indicator.ForeColor = Color.Red;
+        }
+
+
+        /// <summary>
+        /// Room Select to grab beacons
+        /// </summary>
+        /// <param name="sender">dont know</param>
+        /// <param name="e">dont care</param>
+        private void roomSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var a = roomSelect.SelectedIndex;
+
+            uuid.Text = uuids[a];
+            major.Text = majorpair[a];
+            minor.Text = minorpair[a];
+
+            /*switch (roomSelect.SelectedItem)
+            {
+                case "Harrison 004":
+                    uuid.Text = "f0 20 7d a6 4f a2 4e 98 80 24 bc 5b 71 e0 89 3e";
+                    major.Text = "6";
+                    minor.Text = "11";
+                    break;
+                case "Harrison 209":
+                    uuid.Text = "f0 20 7d a6 4f a2 4e 98 80 24 bc 5b 71 e0 89 3e";
+                    major.Text = "6";
+                    minor.Text = "156";
+                    break;
+            }*/
+        }
+
+        private void major_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void minor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void uuid_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!((ch >= 'A' && ch <= 'F') || (ch >= 48 && ch <= 57) || (ch == 32) || (ch >= 'a' && ch <= 'f') || (ch == 127) || (ch == 08) ))
+            e.Handled = true;
+        }
+        
+        private void label2_Click(object sender, EventArgs e)
+        {
+            uuid.Text = Clipboard.GetText();
+        }
+
+        private void roomSelect_Click(object sender, EventArgs e)
+        {
+            roomSelect.DataSource = names;
+        }
+    }
+}
